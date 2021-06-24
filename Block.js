f3486d5a52f81c95bfdc5168b411c46727494588
ranveer0605 <65293345+ranveer0605@users.noreply.github.com>
2021-06-24 17:40:58 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -2,8 +2,7 @@ class Block{
     constructor(x, y, width, height) {
         var options = {
             'restitution':0.8,
-            'friction':0.5,
-            'density':0.5
+            'friction':0,
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
--- Block2.js
+++ Block2.js
@@ -2,8 +2,8 @@ class Block2{
     constructor(x, y, width, height) {
         var options = {
             'restitution':0.8,
-            'friction':0.5,
-            'density':0.5
+            'friction':1.0,
+           
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
